0f47cd9238b82d2fff5f238a41c7cab7668bd328
kongwenshuo <2806144405@qq.com>
2025-01-05 00:53:36 +0800

    实现了PollDispatcher和SelectDispatcher 新增了EventLoop模块里面主要包含Dispatcher DispatcherData 任务队列和ChannelMap和一些线程相关的

--- reactor_http/reactor_http/EventLoop.c
+++ reactor_http/reactor_http/EventLoop.c
@@ -1,2 +1,43 @@
+#include "EventLoop.h"
+#include<assert.h>
 #define _CRT_SECURE_NO_WARNINGS
 
+struct EventLoop* eventLoopInit()
+{
+	return eventLoopInitEx(NULL);
+}
+
+struct EventLoop* eventLoopInitEx(const char* threadName)
+{
+	struct EventLoop* evLoop = (struct EventLoop*)malloc(sizeof(struct EventLoop));
+	evLoop->isQuit = false;
+	evLoop->threadID = pthread_self();
+	pthread_mutex_init(&evLoop->mutex, NULL);
+	strcpy(evLoop->threadName , threadName == NULL ? "MainThread" : threadName);
+	evLoop->dispatcher = &EpollDispatcher;
+	evLoop->dispatcherData = evLoop->dispatcher->init();
+	//链表
+	evLoop->head = evLoop->tail = NULL;
+	//map
+	evLoop->channelMap = channelMapInit(128);
+	return evLoop;
+}
+
+int eventLoopRun(struct EventLoop* evLoop)
+{
+	assert(evLoop != NULL);
+	//取出事件分发和检测模型
+	struct Dispatcher* dispatcher = evLoop->dispatcher;
+	//比较线程id是否正常
+	if (evLoop->threadID == pthread_self())
+	{
+		return -1;
+	}
+
+	//循环事件处理
+	while (!evLoop->isQuit)
+	{
+		dispatcher->dispatch(evLoop, 2);
+	}
+	return 0;
+}
--- reactor_http/reactor_http/EventLoop.h
+++ reactor_http/reactor_http/EventLoop.h
@@ -1,10 +1,45 @@
 #pragma once
-
-extern struct Dispatcher EpollDispatcher;
 #include "Dispatcher.h"
-struct EventLoop
+#include<stdbool.h>
+#include "ChannelMap.h"
+#include <pthread.h>
+extern struct Dispatcher EpollDispatcher;
+extern struct Dispatcher PollDispatcher;
+extern struct Dispatcher SelectDispatcher;
+
+// 如何处理节点中的channel的方式
+enum ElemType{ADD,DELECT,MODIFY};
+
+//定义任务队列的节点
+struct ChannelElement
 {
-	Dispatcher* dispatcher;
-	void* dispatcherData;
+	int type;//如何处理节点中的channel
+	struct Channel* channel;
+	struct ChannelElement* next;
 };
 
+
+struct EventLoop
+{
+	bool isQuit;
+	struct Dispatcher* dispatcher;
+	void* dispatcherData;
+
+	//任务队列
+	struct ChannelElement* head;
+	struct ChannelElement* tail;
+	//map
+	struct ChannelMap* channelMap;
+	//线程id,name,mutex
+	pthread_t threadID;
+	char threadName[32];
+	pthread_mutex_t mutex;
+	 
+};
+
+//初始化
+struct EventLoop* eventLoopInit();//主
+struct EventLoop* eventLoopInitEx(const char* threadName);//子
+
+//启动反应堆模型
+int eventLoopRun(struct EventLoop* evLoop);
